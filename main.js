591139c658ef24909813d8b976813e4f105fc3df
Dawy678 <Dawios2006@wp.pl>
2021-04-30 14:21:59 +0200

    a

---
+++ main.js
@@ -0,0 +1,8 @@
+function wyslij() {
+    let input1 = document.getElementById('input1').value
+    let input2 = document.getElementById('input2').value
+    let input3 = document.getElementById('input3').value
+    let input4 = document.getElementById('input4').value
+    let inputy = `${input1}, ${input2}, ${input3}, ${input4}`
+    console.log(inputy)
+}
